2fd6296e79aec3403392538293a1e936af06d579
AkilaBopage <akilabopage@gmail.com>
2026-07-02 04:18:39 -0700

    fix versal blak page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+
+import { HashRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -9,7 +11,7 @@ import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <BrowserRouter basename="/SRIC">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
