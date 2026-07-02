82bc16d3f69e0e47e7b19973dc89acd4a51ae3eb
AkilaBopage <akilabopage@gmail.com>
2026-07-01 23:29:02 -0700

    change the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,58 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
       <div className="container">
 
+        {/* BRAND */}
         <Link className="navbar-brand fw-bold" to="/">
           SRIC Investors
         </Link>
 
-        <div className="collapse navbar-collapse show">
-          <ul className="navbar-nav ms-auto">
+        {/* HAMBURGER BUTTON */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setOpen(!open)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* MENU */}
+        <div className={`collapse navbar-collapse ${open ? "show" : ""}`}>
+
+          <ul className="navbar-nav ms-auto text-center text-lg-end">
 
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/" onClick={() => setOpen(false)}>
                 Home
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/services">
+              <Link className="nav-link" to="/services" onClick={() => setOpen(false)}>
                 Services
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+              <Link className="nav-link" to="/contact" onClick={() => setOpen(false)}>
                 Contact
               </Link>
             </li>
-             <li className="nav-item">
-              <Link className="nav-link" to="/About">
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/about" onClick={() => setOpen(false)}>
                 About
               </Link>
             </li>
 
-
           </ul>
+
         </div>
 
       </div>
